9fbfa97087327adb51ca28343dbde51473427508
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-07 14:42:36 +0100

    Add game() function and helper functions

--- script.js
+++ script.js
@@ -11,34 +11,105 @@ const getComputerChoice = () => {
   return choice;
 }
 
-const playRound = (playerSelection, computerSelection) => {
-  const playerChoice = playerSelection.toLowerCase();
-  const computerChoice = computerSelection.toLowerCase();
-  let message = null;
+const getPlayerChoice = () => {
+  // Get input from user
+  const playerChoice = prompt("Choose between Rock, Paper, or Scissors");
 
+  return playerChoice;
+}
+
+
+const getCapitalized = (word) => {
+  const firstLetter = word.slice(0, 1);
+  const remainingLetters = word.slice(1);
+  const capitalizedWord = firstLetter.toUpperCase() + remainingLetters.toLowerCase();
+
+  return capitalizedWord;
+}
+
+const playRound = (playerSelection, computerSelection) => {
+  // Capitalize arguments for compatibility
+  const playerChoice = getCapitalized(playerSelection);
+  const computerChoice = getCapitalized(computerSelection);
+  let winner = null;
+
+  // Game rules
   if (playerChoice === computerChoice) {
-    message = `Draw! Both selected ${playerChoice}`;
-  } else if (playerChoice === "paper") {
-    if (computerChoice === "rock") {
-      message = `You win! ${playerChoice} beats ${computerChoice}`;
+    winner = null;
+  } else if (playerChoice === "Paper") {
+    if (computerChoice === "Rock") {
+      winner = 'Player';
     } else {
-      message = `You lose! ${computerChoice} beats ${playerChoice}`;
+      winner = 'Computer';
     }
-  } else if (playerChoice === "rock") {
-    if (computerChoice === "scissors") {
-      message = `You win! ${playerChoice} beats ${computerChoice}`;
+  } else if (playerChoice === "Rock") {
+    if (computerChoice === "Scissors") {
+      winner = 'Player';
     } else {
-      message = `You lose! ${computerChoice} beats ${playerChoice}`;
+      winner = 'Computer';
     }
-  } else if (playerChoice === "scissors") {
-    if (computerChoice === "paper") {
-      message = `You win! ${playerChoice} beats ${computerChoice}`;
+  } else if (playerChoice === "Scissors") {
+    if (computerChoice === "Paper") {
+      winner = 'Player';
     } else {
-      message = `You lose! ${computerChoice} beats ${playerChoice}`;
+      winner = 'Computer';
     }
-  } else {
-    message = 'You can only select between "Rock", "Paper", or "Scissors"';
   }
 
-  return message;
+  return winner;
+}
+
+const game = () => {
+  // Keep track of scores
+  let playerScore = 0;
+  let computerScore = 0;
+
+  // Play 5 rounds
+  for (let round = 0; round < 5; round++) {
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+
+    // Get round winner and show
+    const roundWinner = playRound(playerChoice, computerChoice);
+    showRoundWinner(playerChoice, computerChoice, roundWinner);
+
+    // Check if it was a draw
+    if (roundWinner) {
+      // Increment scores depending on winner
+      if (roundWinner === 'Player') {
+        playerScore++;
+      } else {
+        computerScore++;
+      }
+    }
+  }
+  showGameWinner(playerScore, computerScore);
+}
+
+const showRoundWinner = (playerChoice, computerChoice, winner) => {
+  let message = null;
+
+  if (!winner) {
+    message = `Draw! Both selected ${playerChoice}`;
+  } else {
+    message = `${winner} won this round! ${playerChoice} vs ${computerChoice}`;
+  }
+
+  console.log(message);
+}
+
+const showGameWinner = (playerScore, computerScore) => {
+  let message = null;
+
+  if (playerScore === computerScore) {
+    message = 'This game is a draw!';
+  } else if (playerScore > computerScore) {
+    message = 'The winner is the Player!'
+  } else {
+    message = 'The winner is the Computer';
+  }
+
+  message += ` Player score: ${playerScore} | Computer score: ${computerScore}`
+
+  console.log(message);
 }
