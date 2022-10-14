76f0da50f1631fa9295beaec8a112d9453ff5206
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-14 10:23:01 +0100

    Replace buttons for images

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ import playRound from "./game.js";
 import { getComputerChoice } from "./game.js";
 
 // Get rock, paper, scissors buttons
-const CHOICES = document.querySelectorAll('button');
+const CHOICES = document.querySelectorAll('img');
 
 // Loop through the choices array
 CHOICES.forEach(choice => {
   // Every time the user clicks on the button play a round
   choice.addEventListener('click', () => {
-    const playerChoice = choice.value;
+    const playerChoice = choice.alt;
     const computerChoice = getComputerChoice();
     playRound(playerChoice, computerChoice);
   })
