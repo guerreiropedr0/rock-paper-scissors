21b67b73c39ac32224d43b9b1d5f47b2d651d59e
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-17 00:46:37 +0100

    Add scoreboard to keep up with all games

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+const SCOREBOARD = document.querySelector('#scoreboard');
+const ROUND_SCORE = document.querySelector('#round-score');
+const ROUND_WINNER = document.querySelector('#round-winner');
+
 export const getComputerChoice = () => {
   // Create an array that holds possible choices
   const choices = ['Rock', 'Paper', 'Scissors'];
@@ -49,13 +53,11 @@ const playRound = (playerSelection, computerSelection) => {
   }
 
   showRoundWinner(roundWinner);
-  updateRoundScoreboard(roundWinner);
+  updateGameScore(roundWinner);
   return roundWinner;
 }
 
 const showRoundWinner = (roundWinner) => {
-  const ROUND_WINNER = document.querySelector('#round-winner');
-
   if (roundWinner) {
     ROUND_WINNER.textContent = `${roundWinner} won!`;
   } else {
@@ -64,8 +66,7 @@ const showRoundWinner = (roundWinner) => {
 
 }
 
-const updateRoundScoreboard = (roundWinner) => {
-  const ROUND_SCORE = document.querySelector('#round-score');
+const updateGameScore = (roundWinner) => {
   let playerScore = Number(ROUND_SCORE.innerText[7]);
   let computerScore = Number(ROUND_SCORE.innerText[9]);
 
@@ -83,23 +84,21 @@ const updateRoundScoreboard = (roundWinner) => {
 
   if (gameWinner) {
     showGameWinner(gameWinner);
+    updateScoreboard();
     resetGame();
   }
 }
 
 const resetGame = () => {
-  resetRoundScoreboard();
+  resetGameScore();
   resetRoundInfo();
 }
 
-const resetRoundScoreboard = () => {
-  const ROUND_SCORE = document.querySelector('#round-score');
-
+const resetGameScore = () => {
   ROUND_SCORE.textContent = 'Score: 0-0';
 }
 
 const resetRoundInfo = () => {
-  const ROUND_WINNER = document.querySelector('#round-winner');
   ROUND_WINNER.textContent = '';
 }
 
@@ -123,6 +122,29 @@ const showGameWinner = (gameWinner) => {
   }
 
   alert(message);
+}
+
+const updateScoreboard = () => {
+  const NUMBER_OF_GAMES = getGamesCount();
+  const PLAYER_SCORE = Number(ROUND_SCORE.innerText[7]);
+  const COMPUTER_SCORE = Number(ROUND_SCORE.innerText[9]);
+
+
+  SCOREBOARD.innerHTML += `<div class="scoreboard-item">Game ${NUMBER_OF_GAMES}</div><div class="scoreboard-item">${PLAYER_SCORE}</div><div class="scoreboard-item">${COMPUTER_SCORE}</div>`;
+}
+
+const getGamesCount = () => {
+  const SCOREBOARD_CHILDREN = SCOREBOARD.children.length;
+
+  // Subtract the initial 3 scoreboard childs (SCOREBOARD, COMPUTER, PLAYER)
+  let NUMBER_OF_GAMES = SCOREBOARD_CHILDREN - 2;
+
+  // For every game we add, we add 2 more children (3 children) so divide by 3
+  if (SCOREBOARD_CHILDREN > 3) {
+    NUMBER_OF_GAMES = Math.ceil(NUMBER_OF_GAMES / 3);
+  }
+
+  return NUMBER_OF_GAMES;
 }
 
 export default playRound;
